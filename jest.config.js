718bc2faafe9de35505e4354e42bcb96d9c795fc
Frieder Bluemle <frieder.bluemle@gmail.com>
2025-10-11 21:21:18 -0700

    Add base RN dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-native-animatable|react-native-modal|react-native-safe-area-context|react-native-url-polyfill)/)',
+  ],
 };
---
+++ jest.setup.js
@@ -0,0 +1,12 @@
+/* eslint-env jest */
+
+import mockRNCNetInfo from '@react-native-community/netinfo/jest/netinfo-mock';
+import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
+
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
+
+jest.mock('@react-native-community/netinfo', () => mockRNCNetInfo);
+
+jest.mock('react-native-safe-area-context', () => mockSafeAreaContext);
