3268601a7184a2c2c05ba1c373082c91609f46de
Frieder Bluemle <frieder.bluemle@gmail.com>
2025-10-14 11:37:26 -0400

    Add react-native-svg-transformer

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\\.svg$': 'jest-transformer-svg',
+  },
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-native-animatable|react-native-modal|react-native-safe-area-context|react-native-url-polyfill)/)',
   ],
